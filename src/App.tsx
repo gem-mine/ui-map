8226e9ec5e061289c9ef1f03d180944ce12307a5
GYL <sehun@0412.com>
2020-12-17 15:26:16 +0800

    init by gem-mine 👻

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import EgComponent from './export'
+
+export default class App extends React.Component {
+  render(): JSX.Element {
+    return (
+      <EgComponent />
+    )
+  }
+}
---
+++ src/export/i18n/language.ts
@@ -0,0 +1,8 @@
+export default {
+  'zh-CN': {
+    label: '简体中文'
+  },
+  en: {
+    label: 'English'
+  }
+}
